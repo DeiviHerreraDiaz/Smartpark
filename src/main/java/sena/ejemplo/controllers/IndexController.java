abc0ebc99e03d13358089b72ce7ab6dbb4f80b75
DeiviHerreraDiaz <128943900+DeiviHerreraDiaz@users.noreply.github.com>
2023-11-25 17:52:06 -0500

    Project Complete v2

--- src/main/java/sena/ejemplo/controllers/IndexController.java
+++ src/main/java/sena/ejemplo/controllers/IndexController.java
@@ -1,12 +1,20 @@
 package sena.ejemplo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sena.ejemplo.model.Usuario;
+import sena.ejemplo.service.IUsuarioService;
 
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private IUsuarioService usuarioService;
 
     @GetMapping(value = "/")
     public String inicio() {
@@ -21,8 +29,13 @@ public class IndexController {
     }
 
     @GetMapping(value = "/menu")
-    public String menu() {
+    public String menu(Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        model.addAttribute("usuarioAutenticado", auth.getName()); // Esto asume que el nombre del usuario es relevante para tu caso
+        Usuario usuario = usuarioService.findByDocumento(auth.getName());
 
+        model.addAttribute("nombreUsuario", usuario.getNombre());
+        model.addAttribute("apellidoUsuario", usuario.getApellido());
 
         return "/menu";
     }
